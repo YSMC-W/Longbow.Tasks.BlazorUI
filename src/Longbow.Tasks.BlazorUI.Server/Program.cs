4ed90f9fa437060aa15bba253381f5d933de6d7e
wgh <wgh@zyjinke.com>
2024-05-06 23:26:40 +0800

    build: Initialize

---
+++ src/Longbow.Tasks.BlazorUI.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using Longbow.Tasks.BlazorUI.Server.Components;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+builder.Services.AddTaskServices();
+
+// 增加 SignalR 服务数据传输大小限制配置
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
